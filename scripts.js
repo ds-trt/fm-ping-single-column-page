ad93570d40d5507db15f504d95d727eca1640161
sdorbin <dis_tort@yahoo.com>
2020-10-14 22:42:17 -0700

    updated js to display seperate validation messages for no email and invalid email cases.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,12 @@ const error_msg = document.querySelector('.notify__error');
 
 function validateEmail(e) { 
     e.preventDefault();
-    if(email.validity.typeMismatch || email.validity.valueMissing) {
+    if(email.validity.typeMismatch) {
+        error_msg.textContent = 'Please provide a valid email address';
+        error_msg.classList.remove('notify__error--hidden');
+        email.classList.add('notify__email--error');
+    } else if(email.validity.valueMissing) {
+        error_msg.textContent = 'Whoops! It looks like you forgot to add your email';
         error_msg.classList.remove('notify__error--hidden');
         email.classList.add('notify__email--error');
     } else {
